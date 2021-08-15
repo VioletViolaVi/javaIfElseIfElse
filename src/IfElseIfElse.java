79643bee7b2924628e305cdc0f5f4df7975409bc
VioletViola <vivian.v18723@gmail.com>
2021-08-15 23:58:34 +0100

    Practice time using if, else if and else statements.

--- src/IfElseIfElse.java
+++ src/IfElseIfElse.java
@@ -12,6 +12,34 @@ public class IfElseIfElse {
             System.out.println("Invalid age!");
         }
 
+        // 1-12 o'clock
+        int timeOfDay = 12;
+        if (timeOfDay >= 1 && timeOfDay <= 4) {
+            System.out.println("Good Night!");
+        } else if (timeOfDay >= 5 && timeOfDay <= 11) {
+            System.out.println("Good Morning!");
+        } else {
+            System.out.println("Good Afternoon!");
+        }
 
+        // 24hr clock
+        int time = 19;
+        if (time >= 0 && time <= 4) {
+            System.out.println("Good EARLY Morning! - " + "0" + time + ":00");
+        } else if (time >= 5 && time <= 11) {
+            if (time >= 5 && time <= 9) {
+                System.out.println("Good Morning! - " + "0" + time + ":00");
+            } else {
+                System.out.println("Good Morning! - " + time + ":00");
+            }
+        } else if (time >= 12 && time <= 16) {
+            System.out.println("Good Afternoon! - " + time + ":00");
+        } else if (time >= 17 && time <= 20) {
+            System.out.println("Good Evening! - " + time + ":00");
+        } else if (time >= 21 && time <= 23) {
+            System.out.println("Good Night! - " + time + ":00");
+        } else {
+            System.out.println("Invalid time!");
+        }
     }
 }
